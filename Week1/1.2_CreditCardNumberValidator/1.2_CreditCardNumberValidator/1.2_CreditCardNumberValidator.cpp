e178433e06dbbb09ec072acf2ca396064a44cab3
kwint <kwintjanleenders@gmail.com>
2025-04-22 11:53:38 +0200

    Assignment 1.2 done

--- Week1/1.2_CreditCardNumberValidator/1.2_CreditCardNumberValidator/1.2_CreditCardNumberValidator.cpp
+++ Week1/1.2_CreditCardNumberValidator/1.2_CreditCardNumberValidator/1.2_CreditCardNumberValidator.cpp
@@ -1,20 +1,84 @@
-// 1.2_CreditCardNumberValidator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+bool isValid(const std::string& cardNumber);
+int sumOfDoubleEvenPlace(const std::string& cardNumber);
+int getDigit(int number);
+int sumOfOddPlace(const std::string& cardNumber);
+bool startsWith(const std::string& cardNumber, const std::string& substr);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string cardNumber;
+    std::cout << "Write the number of your card\n";
+    std::cin >> cardNumber;
+
+    isValid(cardNumber);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Return true if the card number is valid
+bool isValid(const std::string& cardNumber) 
+{
+    if (!(startsWith(cardNumber, "4") ||
+        startsWith(cardNumber, "5") ||
+        startsWith(cardNumber, "6") ||
+        startsWith(cardNumber, "37")))
+    {
+        std::cout << "The number is invalid, it doesnt start with a proper integer";
+        return false;
+    }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    if (cardNumber.length() < 13 || cardNumber.length() > 16) {
+        std::cout << "The number is invalid, it's not the right size (between 13 and 16 numbers)";
+        return false;
+    }
+
+    int result = sumOfDoubleEvenPlace(cardNumber) + sumOfOddPlace(cardNumber);
+    if (result % 10 == 0) {
+        std::cout << result << " is a valid number!";
+    }
+    else
+    {
+        std::cout << result << " is not valid number!";
+    }
+}
+
+// Get the result from Step 2
+int sumOfDoubleEvenPlace(const std::string& cardNumber) 
+{
+    int total = 0;
+    for (int i = cardNumber.length() - 2; i >= 0; i -= 2) {
+        int digit = cardNumber.at(i) - '0';
+        total += getDigit(digit * 2);
+    }
+    std::cout << "Sum of doubled even-place digits: " << total << std::endl;
+    return total;
+}
+
+// Return this number if it is a single digit, otherwise,
+// return the sum of the two digits
+int getDigit(int number) {
+    if (number < 10) {
+        return number;
+    }
+    else {
+        return ((number / 10) + (number % 10));
+    }
+}
+
+// Return sum of odd-place digits in the card number
+int sumOfOddPlace(const std::string& cardNumber) {
+    int total = 0;
+    for (int i = cardNumber.length() - 1; i >= 0; i -= 2) {
+        int digit = cardNumber.at(i) - '0';
+        total += digit;
+    }
+    std::cout << "Sum of odd-place digits: " << total << std::endl;
+    return total;
+}
+
+// Return true if substr is the prefix for cardNumber
+bool startsWith(const std::string& cardNumber, const std::string& substr) {
+    return cardNumber.size() >= substr.size() &&
+        cardNumber.substr(0, substr.size()) == substr;
+}
